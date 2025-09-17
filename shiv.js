676951b6aefb50005a7ee53714d8f30a451ecca2
KH DEEKSHITH REDDY <deekshithreddy371@gmail.com>
2025-09-17 07:23:31 +0530

    Add files via upload

---
+++ shiv.js
@@ -0,0 +1,5 @@
+// Basic contact form validation
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert('Thank you for your message! I\'ll get back soon.');
+});
